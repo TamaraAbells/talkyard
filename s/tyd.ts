29237cb6fe97f4769ff6065d2b3d7ab6e3794d90
Kaj Magnus Lindberg <kajmagnus3@gmail.com>
2020-11-03 22:43:23 +0100

    w.

--- s/tyd.ts
+++ s/tyd.ts
@@ -2,7 +2,8 @@ import * as _  from 'lodash';
 import * as minimist from 'minimist';
 import Launcher from '@wdio/cli'
 import * as glob from 'glob';
-import { die, dieIf } from '../tests/e2e/utils/log-and-die';
+import { die, dieIf, logMessage, logMessageIf, logErrorIf, logUnusual
+            } from '../tests/e2e/utils/log-and-die';
 import { ChildProcess, spawn as _spawnAsync, spawnSync as _spawnSync } from 'child_process';
 
 
@@ -34,7 +35,7 @@ import { ChildProcess, spawn as _spawnAsync, spawnSync as _spawnSync } from 'chi
 
 
 //function spawnInBackground(cmd: string, args: string[], how?: 'Detach' | 'InForeground') {
-function spawnInBackground(cmd: string, args: string[]): ChildProcess {
+function spawnInBackground(cmd: St, args: St[]): ChildProcess {
   console.log(`Spawn:  ${cmd} ${args.join(' ')}`);
   //if (how === 'Detach') {
     return _spawnAsync(cmd, args, { detached: true, stdio: 'inherit' });
@@ -51,7 +52,7 @@ function spawnInBackground(cmd: string, args: string[]): ChildProcess {
 }
 
 
-function spawnInForeground(cmd: string, args: string[]) {
+function spawnInForeground(cmd: St, args: St[]) {
   console.log(`Spawn fg:  ${cmd} ${args.join(' ')}`);
   // stdio 'inherit' makes the child process write directly to our stdout,
   // so colored output and CTRL+C works.
@@ -77,6 +78,9 @@ if (mainCmd === 'justwatch') {
 
 if (mainCmd === 'watchup') {
   const watchChildProcess = spawnInBackground('make', ['watch']);
+  // RACE BUG when starting, we'll run  logs -f  too soon,
+  // before any containers have started. Seems it then exits and
+  // shuts down  watch & the log tailing.
   spawnInBackground('docker-compose', ['up', '-d']);
   spawnInForeground('docker-compose', ['logs', '-f', '--tail', '0']);
   watchChildProcess.kill();
@@ -109,64 +113,17 @@ if (mainCmd !== 'e2e') {
 
 
 const e2eSpecsPattern = `tests/e2e/specs/${'*' + (subCmd || '') + '*.ts' }`;
-console.log(`Running tests?: ${e2eSpecsPattern} ...`);
 
-const files = glob.sync(e2eSpecsPattern, {});
+const allMatchingSpecs = glob.sync(e2eSpecsPattern, {});
 
-console.log(`Files:\n - ${files.join('\n - ')}`);
+console.log(`Specs pattern: ${e2eSpecsPattern}`);
+console.log(`Specs matching:\n - ${allMatchingSpecs.join('\n - ')}`);
 
-const args = {
-  ...cmdLineArgs,
-  //specs: ['tests/e2e/specs/manual.2browsers.test.ts'],
-  specs: files.length ? files : [
-    'tests/e2e/specs/all-links.test.ts',
-    'tests/e2e/specs/api-list-query-for-posts.test.ts',
-    'tests/e2e/specs/api-list-query-for-topics.test.ts',
-    'tests/e2e/specs/api-search-full-text.test.ts',
-    'tests/e2e/specs/authz-basic-see-reply-create.test.ts',
-    'tests/e2e/specs/authz-view-as-stranger.test.ts',
-    'tests/e2e/specs/create-site-facebook.test.ts',
-    'tests/e2e/specs/create-site-github-oauth-uppercase-email.test.ts',
-    'tests/e2e/specs/create-site-gmail-and-email-notf.test.ts',
-    'tests/e2e/specs/create-site-linkedin.test.ts',
-    'tests/e2e/specs/create-site-password-run-admin-intro-tours.test.ts',
-    'tests/e2e/specs/drafts-delete.test.ts',
-    'tests/e2e/specs/drafts-new-topic-from-cats-page.test.ts',
-    'tests/e2e/specs/emb-com.all-logins.1br.test.ts',
-    'tests/e2e/specs/emb-com.all-logins-old-name.1br.test.ts',
-    'tests/e2e/specs/embed-images-mp4-youtube.test.ts',
-    'tests/e2e/specs/embed-twitter-tweets-etc.test.ts',
-    'tests/e2e/specs/impersonate-restricted-areas.test.ts',
-    'tests/e2e/specs/link-previews-all-others.test.ts',
-    'tests/e2e/specs/login-required-oauth-signup-login.test.ts',
-    'tests/e2e/specs/navigation-as-admin.test.ts',
-    'tests/e2e/specs/navigation-as-member.test.ts',
-    'tests/e2e/specs/navigation-as-stranger.test.ts',
-    'tests/e2e/specs/oauth-signup-signin.test.ts',
-    'tests/e2e/specs/page-type-discussion-progress.test.ts',
-    'tests/e2e/specs/page-type-idea-statuses-comments.test.ts',
-    'tests/e2e/specs/page-type-info-page.test.ts',
-    'tests/e2e/specs/page-type-problem-statuses.test.ts',
-    'tests/e2e/specs/sso-admin-extra-login.test.ts',
-    'tests/e2e/specs/upload-images-and-files.test.ts',
-    'tests/e2e/specs/user-profile-access.test.ts',
-    'tests/e2e/specs/user-profile-cannot-delete-openauth-email.test.ts',
-    'tests/e2e/specs/user-profile-change-username.test.ts',
-    'tests/e2e/specs/utx-all-logins.test.ts',
-    'tests/e2e/specs/votes-and-best-first.test.ts',
-  ],
-};
-
-//DUPL CODE for now [DUPWDIOCNF]
-// Depending on which e2e tests to run, amend `args` so we'll use the
-// correct capabilities and static files help server.
-// 
-// `args` gets merged into the WebdriverIO.Config returned by wdio.conf.ts.
-// So, args.specs gest aded to? overrides? the config.spec in wdio.conf.ts.
 
 // Can look at node_modules/@wdio/cli/build/launcher.js  to see
 // ex of how handle async errs?
-async function runE2eTests(): Promise<number> {
+async function runE2eTests(): Promise<Nr> {
+  let zeroOrFirstErrorCode: U | Nr;
 
   // Command line arguments and the test runners?
   //
@@ -175,23 +132,63 @@ async function runE2eTests(): Promise<number> {
   // fork() with: { cwd: process.cwd(), env: runnerEnv, execArgv: this.execArgv },
   // see: ../node_modules/@wdio/local-runner/build/worker.js
 
-  function shallRun(...testTypes: string[]) {
+  async function specsMatching(testTypes: St[], run: (specs: St[]) => Promise<Nr>) {
     dieIf(!testTypes?.length, 'TyE38590RTK');
+    let specsNow = allMatchingSpecs;
     for (let tt of testTypes) {
-      const found = !!files.find((fileName: string) => fileName.indexOf(`.${tt}.`) >= 0);
-      if (!found) return false;
+      specsNow = specsNow.filter((fileName: St) => {
+        // 0 like Nothing, Not, Skip, Exclude. But '!' would need to be shell
+        // escaped, one needs to type '\!', right? An extra char, how boring.
+        const shallInclude = tt[0] !== '0';
+        const matchesType = fileName.indexOf(`.${tt}.`) >= 0;
+        return matchesType === shallInclude;
+      });
     }
-    return true;
+    if (specsNow.length) {
+      const sep = '\n - ';
+      const what = '.' + testTypes.join('.');
+      logMessage(`Running ${what} specs:${sep}${specsNow.join[sep]}`);
+      const exitCode = await run(specsNow);
+      if (!zeroOrFirstErrorCode) {
+        zeroOrFirstErrorCode = exitCode;
+      }
+      logErrorIf(exitCode !== 0, `ERROR exit code ${exitCode} from:  ${what}`);
+      logMessageIf(exitCode === 0, `Done, fine, exit code 0 from:  ${what}`);
+    }
   }
 
-  const run1BrIdpAuth = shallRun('1br', 'idpauth');
-  const runIdpAuth = shallRun('idpauth');
+  //DUPL CODE for now [DUPWDIOCNF]
 
-  // ...
+  // Run all variants (e.g. 1br, 2br, 3br) — so we'll find all failing tests
+  // without having to restart over and over again:
+  //
+  // Depending on which e2e tests to run, amend `args` so we'll use the
+  // correct capabilities and static files help server.
+  //
+  // `args` gets merged into the WebdriverIO.Config returned by wdio.conf.ts.
+  // So, args.specs gest aded to? overrides? the config.spec in wdio.conf.ts.
 
-  const wdio = new Launcher('./tests/e2e/wdio.conf.js', args);
-  const code = await wdio.run();
-  return code;
+  await specsMatching(['1br'], async (specs: St[]): Promise<Nr> => {
+    const args = { specs }
+    const wdio = new Launcher('./tests/e2e/wdio.conf.js', args);
+    return wdio.run();
+  });
+
+  await specsMatching(['2br'], async (specs: St[]): Promise<Nr> => {
+    die('2br not impl [TyE390DRETSK2]')
+    const args = { specs }
+    const wdio = new Launcher('./tests/e2e/wdio.conf.js', args);
+    return await wdio.run();
+  });
+
+  await specsMatching(['3br'], async (specs: St[]): Promise<Nr> => {
+    die('3br not impl [TyE390DRETSK3]')
+    const args = { specs }
+    const wdio = new Launcher('./tests/e2e/wdio.conf.js', args);
+    return await wdio.run();
+  });
+
+  return zeroOrFirstErrorCode;
 }
 
 
@@ -200,8 +197,9 @@ console.log(`Running e2e tests ...`);
 runE2eTests().then((code) => {
   const fineOrFailed = code === 0 ? 'fine' : 'tests FAILED';
   console.log(`Done running e2e tests, exit code: ${code}, ${fineOrFailed}`);
+  logErrorIf(code === undefined, `Error: Didn't run any tests at all [TyE0SPECSRUN]`);
   process.exit(code);
 }, (error) => {
-  console.error(`Error starting tests`, error.stacktrace);
+  console.error(`Error starting tests [TyEE2ESTART]`, error.stacktrace);
   process.exit(1);
 });
--- tests/e2e/wdio.conf.js
+++ tests/e2e/wdio.conf.js
@@ -3,22 +3,21 @@
 // require('ts-node/register')
 
 require('ts-node').register({
-      transpileOnly: true,
-      // So ts-node finds the tsconfig.json for the e2e tests
-      // — it specifies target: 'ES2017', otherwise, with ES2015 (the default),
-      // there's this error:
-      dir: './tests/e2e/',
-      /*
-      compilerOptions : {
-        // Without this, ts-node apparently transpiles to ES2015,
-        // causing this error:
-        //   > TypeError: Class constructor WDIOReporter cannot be invoked without 'new'
-        // See:
-        // https://stackoverflow.com/questions/51860043/
-        //    javascript-es6-typeerror-class-constructor-client-cannot-be-invoked-without-ne
-        target: 'ES2017',
-      }, */
-    });
+  transpileOnly: true,
+
+  // Help ts-node find the tsconfig.json for the e2e tests — that config file
+  // specifies target: 'ES2017', otherwise, with ES2015 (the default),
+  // there's this error:
+  //   > TypeError: Class constructor WDIOReporter cannot be invoked without 'new'
+  // See:
+  // https://stackoverflow.com/questions/51860043/
+  //    javascript-es6-typeerror-class-constructor-client-cannot-be-invoked-without-ne
+  //
+  // Is it odd that the wdio.conf.ts dir isn't the default, for looking up
+  // the tsconfig.json? Instead, the process' current working dir, is.
+  //
+  dir: './tests/e2e/',
+});
 
 // Include the '.ts' suffix, otherwise apparently any '.js' file with the same
 // name (excl suffix) gets loaded.
